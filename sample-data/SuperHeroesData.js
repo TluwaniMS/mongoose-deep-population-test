be8051f2709738c6aa044edaf2478107c5593d0f
tluwaniMS <tluwani2020@gmail.com>
2022-03-12 16:56:11 +0200

    adding-sample-data

---
+++ sample-data/SuperHeroesData.js
@@ -0,0 +1,7 @@
+const SuperHeroes = [
+  { _id: "61f58a1d2f87a0e538236b28", name: "Elephant Man" },
+  { _id: "61f58a1d2f87a0e538236b29", name: "Imhotep" },
+  { _id: "61f58a1d2f87a0e538236b2a", name: "Shaka Khan" }
+];
+
+module.exports = { SuperHeroes };
